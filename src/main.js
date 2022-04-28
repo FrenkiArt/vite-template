c1f4703d2bd2a95b6594c61287c0ddaeac066e80
frenkiart <frenki_art@mail.ru>
2022-04-28 09:22:28 +0300

    feat: update

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 // bootstrap
-import 'bootstrap/scss/bootstrap-grid.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
-// uikit
-import 'uikit/dist/css/uikit.css';
-import 'uikit/dist/js/uikit.js';
-import 'uikit/dist/js/uikit-icons.js';
+import "bootstrap/scss/bootstrap-grid.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 // import Swiper JS
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from "swiper";
 // import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
@@ -23,16 +18,16 @@ window.Swiper = Swiper;
 // import './fonts/stylesheet.css';
 
 // my components
-import './components/template/component.scss';
-import './components/template/component.js';
+import "./components/template/component.scss";
+import "./components/template/component.js";
 
 // my
-import './scss/style.scss';
-import './scss/header.scss';
+import "./scss/style.scss";
+import "./scss/header.scss";
 
-import './scss/footer.scss';
-import './scss/stuff.scss';
-import './scss/button.scss';
-import './scss/added-styles.scss';
+import "./scss/footer.scss";
+import "./scss/stuff.scss";
+import "./scss/button.scss";
+import "./scss/added-styles.scss";
 
-import './js/my.js';
+import "./js/my.js";
