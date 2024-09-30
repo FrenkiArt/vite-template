0c4b33577985d5de53543dfeadd5c3ede01605d5
Arthur <Frenki_art@mail.ru>
2024-09-30 19:16:41 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 // import Swiper styles
 import 'swiper/css/bundle';
+//import 'swiper/css';
+//import 'swiper/css/autoplay';
+//import 'swiper/css/controller';
+//import 'swiper/css/effect-fade';
+//import 'swiper/css/free-mode';
+//import 'swiper/css/grid';
+//import 'swiper/css/hash-navigation';
+//import 'swiper/css/history';
+//import 'swiper/css/keyboard';
+//import 'swiper/css/navigation';
+//import 'swiper/css/pagination';
+//import 'swiper/css/scrollbar';
+//import 'swiper/css/thumbs';
 
 import 'lightgallery/css/lightgallery.css';
 //import 'lightgallery/css/lg-fullscreen.css';
