ec19e732a8cdef016de30b99c533ad3999b35413
Arthur <31354771+FrenkiArt@users.noreply.github.com>
2025-04-05 01:02:31 +0300

    update

--- src/main.js
+++ src/main.js
@@ -25,26 +25,22 @@ import './assets/styles/style.scss';
 import './assets/styles/header.scss';
 import './assets/styles/sections.scss';
 import './assets/styles/footer.scss';
-
-import './assets/styles/breadcrumb.scss';
+ 
 import './assets/styles/typograf.scss';
 import './assets/styles/links.scss';
 import './assets/styles/buttons.scss';
 import './assets/styles/nav.scss';
+import './assets/styles/details.scss';
 
 import './assets/styles/card.scss';
 import './assets/styles/stuff.scss';
 import './assets/styles/added-styles.scss';
 
-//import './assets/styles/shortened.scss';
-//import './assets/styles/acc.scss';
+//import './assets/styles/shortened.scss'; 
 //import './assets/styles/pagination.scss';
-//import './assets/styles/popup.scss';
+ 
 
-//import './assets/js/tabs.js';
-//import './assets/js/acc.js';
-//import './assets/js/shortened.js';
-//import './assets/js/popup.js';
-//import './assets/js/youtube.js';
+//import './assets/js/tabs.js'; 
+//import './assets/js/shortened.js'; 
 
 import './assets/js/my.js';
